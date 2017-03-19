09f03be3658afbbcd4baa8193af35a40cc7a879d
Mike Pennisi <mike@mikepennisi.com>
2017-03-19 17:30:12 -0400

    Create new kind of reproduction

--- eventsource/resources/status-error.py
+++ eventsource/resources/status-error.py
@@ -1,8 +1,24 @@
+import time
+body = 'This should not be'
 template = '''
-HTTP/1.1 204 HAHAHAHA
+HTTP/1.1 200 HAHAHAHA
 Content-Type: text/event-stream
-'''.strip() + '\n\n'
+Content-Length: %s
+
+'''.lstrip() % (len(body))
 
 def main(request, response):
-  response.writer.write('invalid')
+  #request_id_1 = request.GET.first('request_id_1')
+  print response.writer._handler
+  return ''
+  if request.GET.first('foo', '0') == '1':
+      request.server.stash.put(token, 'something')
+      return ''
   response.writer.write(template)
+  response.writer.flush()
+
+  while request.server.stash.take(token) == None:
+      time.sleep(1)
+      pass
+  response.writer.write(body)
+  response.writer.flush()
