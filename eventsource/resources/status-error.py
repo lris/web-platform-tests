12a854f94cf6cccc5065f565f09deda552c6ae30
Mike Pennisi <mike@mikepennisi.com>
2017-03-20 17:05:23 -0400

    Continue experimenting

--- eventsource/resources/status-error.py
+++ eventsource/resources/status-error.py
@@ -1,26 +1,12 @@
-import time
-body = 'This should not be'
+body = 'console.log(1);'
 template = '''
 HTTP/1.1 204 HAHAHAHA
-Content-Type: text/event-stream
+Content-Type: applicatiThis should not beon/javascript
 Content-Length: %s
 
-'''.lstrip() % (len(body))
+%s'''.lstrip() % (len(body), body)
 
 def main(request, response):
-  #request_id_1 = request.GET.first('request_id_1')
-  token = request.GET.first('token', '')
-
-  if request.GET.first('foo', '0') == '1':
-      request.server.stash.put(token, 'something')
-      return ''
   response.writer.write(template)
   response.writer.flush()
 
-  #while request.server.stash.take(token) == None:
-  #    time.sleep(1)
-  #    pass
-  #print dir(response.writer._handler)
-  response.writer._handler.close_connection = True
-  #response.writer.write(body)
-  #response.writer.flush()
