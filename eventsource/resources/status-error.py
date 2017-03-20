0ac8dd87f256f0b82f9d1bdb1294f329f7c18f82
Mike Pennisi <mike@mikepennisi.com>
2017-03-20 12:10:32 -0400

    More stuff

--- eventsource/resources/status-error.py
+++ eventsource/resources/status-error.py
@@ -1,7 +1,7 @@
 import time
 body = 'This should not be'
 template = '''
-HTTP/1.1 200 HAHAHAHA
+HTTP/1.1 204 HAHAHAHA
 Content-Type: text/event-stream
 Content-Length: %s
 
@@ -9,16 +9,18 @@ Content-Length: %s
 
 def main(request, response):
   #request_id_1 = request.GET.first('request_id_1')
-  print response.writer._handler
-  return ''
+  token = request.GET.first('token', '')
+
   if request.GET.first('foo', '0') == '1':
       request.server.stash.put(token, 'something')
       return ''
   response.writer.write(template)
   response.writer.flush()
 
-  while request.server.stash.take(token) == None:
-      time.sleep(1)
-      pass
-  response.writer.write(body)
-  response.writer.flush()
+  #while request.server.stash.take(token) == None:
+  #    time.sleep(1)
+  #    pass
+  #print dir(response.writer._handler)
+  response.writer._handler.close_connection = True
+  #response.writer.write(body)
+  #response.writer.flush()
