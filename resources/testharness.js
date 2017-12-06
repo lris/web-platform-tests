0356de2b71e65e887ba42a721dd4f359aef92813
Mike Pennisi <mike@mikepennisi.com>
2017-12-06 11:47:12 -0500

    Fix Promise support

--- resources/testharness.js
+++ resources/testharness.js
@@ -1640,10 +1640,7 @@ policies and contribution forms [3].
     {
         var this_obj = this;
         if (this.phase >= this.phases.CLEANING) {
-            if (this.phase === this.phases.CLEANING) {
-				this.set_status(this.PASS, null);
-			}
-			return;
+		    return;
 		}
 
         if (this.phase <= this.phases.STARTED) {
@@ -1888,7 +1885,8 @@ policies and contribution forms [3].
 		Object.defineProperty(this, 'status', {
 			set(value) {
 			  _status = value;
-			  console.log('setting tests.status', value, this);
+			  console.log('setting tests.status to:', value);
+			  console.log(JSON.stringify(TestsStatus.statuses, null, 2));
 			},
 			get() { return _status; }
 		});
@@ -2062,7 +2060,6 @@ policies and contribution forms [3].
 			} else {
 			    this.status.status = this.status.TIMEOUT;
 			}
-			console.log(this.status);
         }
         this.complete();
     };
